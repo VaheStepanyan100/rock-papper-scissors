745bdf4915fe3488c44bf04ae99c3bc62a84bc81
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-02-18 17:04:40 +0400

    Create function that plays single round

--- app.js
+++ app.js
@@ -3,3 +3,39 @@ function getComputerChoice() {
   const lengthOfShapes = arrOfshapes.length;
   return arrOfshapes[Math.floor(Math.random() * lengthOfShapes)];
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return `Draw! You both picked ${computerSelection}`;
+  } else if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection.toLowerCase() === "paper"
+  ) {
+    return `You Lose! Paper beats Rock`;
+  } else if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection.toLowerCase() === "scissors"
+  ) {
+    return `You Win! Rock beats Scissors`;
+  } else if (
+    playerSelection.toLowerCase() === "paper" &&
+    computerSelection.toLowerCase() === "rock"
+  ) {
+    return `You Win! Paper beats Rock`;
+  } else if (
+    playerSelection.toLowerCase() === "paper" &&
+    computerSelection.toLowerCase() === "scissors"
+  ) {
+    return `You Lose! Scissors beats Paper`;
+  } else if (
+    playerSelection.toLowerCase() === "scissors" &&
+    computerSelection.toLowerCase() === "rock"
+  ) {
+    return `You Lose! Rock beats Scissors`;
+  } else if (
+    playerSelection.toLowerCase() === "scissors" &&
+    computerSelection.toLowerCase() === "paper"
+  ) {
+    return `You Win! Scissors beats Paper`;
+  }
+}
